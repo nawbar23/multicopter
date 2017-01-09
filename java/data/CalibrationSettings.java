979f7e841fe5c87debb44e49831d0091691e6409
Bartosz Nawrot <nawbar23@gmail.com>
2017-01-09 20:59:43 +0100

    Calibration settings serialization fixed

--- java/data/CalibrationSettings.java
+++ java/data/CalibrationSettings.java
@@ -5,64 +5,79 @@ import com.multicopter.java.CommMessage;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * Created by ebarnaw on 2016-10-14.
  */
 public class CalibrationSettings implements SignalPayloadData {
 
-    private Data data;
+    private  float[] gyroOffset; // Vect3Df
+    private float[] accelCalib; // Mat3Df
+    private float[] magnetSoft; // Mat3Df
+    private float[] magnetHard; // Vect3Df
+    private  float altimeterSetting;
+    private float temperatureSetting;
+    private float[] radioLevels; // Mat4Df
+    private byte[] pwmInputLevels; // 8 params
+    private int boardType;
     private Flags flags;
+    private int crcValue;
 
     public CalibrationSettings() {
-        this.data = new Data();
-        flags = new Flags(32, data.flagsValue);
-        data.accelCalib[0] = 1.0f;
-        data.accelCalib[4] = 1.0f;
-        data.accelCalib[8] = 1.0f;
+        gyroOffset = new float[3];
+        accelCalib = new float[9];
+        magnetSoft = new float[9];
+        magnetHard = new float[3];
+        radioLevels = new float[16];
+        pwmInputLevels = new byte[8];
+        flags = new Flags(32);
 
-        data.magnetSoft[0] = 1.0f;
-        data.magnetSoft[4] = 1.0f;
-        data.magnetSoft[8] = 1.0f;
+        accelCalib[0] = 1.0f;
+        accelCalib[4] = 1.0f;
+        accelCalib[8] = 1.0f;
 
-        data.altimeterSetting = 1013.2f;
-        data.temperatureSetting = 288.15f;
+        magnetSoft[0] = 1.0f;
+        magnetSoft[4] = 1.0f;
+        magnetSoft[8] = 1.0f;
 
-        data.crcValue = CommMessage.computeCrc32(data.serialize());
+        altimeterSetting = 1013.2f;
+        temperatureSetting = 288.15f;
+        crcValue = computeCrc();
+
+        System.out.println(crcValue);
     }
 
     public CalibrationSettings(final byte[] dataArray) {
         ByteBuffer buffer = ByteBuffer.wrap(dataArray);
         buffer.order(ByteOrder.LITTLE_ENDIAN);
 
-        this.data = new Data();
-        for (int i = 0; i < data.gyroOffset.length; i++) {
-            data.gyroOffset[i] = buffer.getFloat();
+        for (int i = 0; i < gyroOffset.length; i++) {
+            gyroOffset[i] = buffer.getFloat();
         }
-        for (int i = 0; i < data.accelCalib.length; i++) {
-            data.accelCalib[i] = buffer.getFloat();
+        for (int i = 0; i < accelCalib.length; i++) {
+            accelCalib[i] = buffer.getFloat();
         }
-        for (int i = 0; i < data.magnetSoft.length; i++) {
-            data.magnetSoft[i] = buffer.getFloat();
+        for (int i = 0; i < magnetSoft.length; i++) {
+            magnetSoft[i] = buffer.getFloat();
         }
-        for (int i = 0; i < data.magnetHard.length; i++) {
-            data.magnetHard[i] = buffer.getFloat();
-        }
-
-        data.altimeterSetting = buffer.getFloat();
-
-        for (int i = 0; i < data.radioLevels.length; i++) {
-            data.radioLevels[i] = buffer.getFloat();
-        }
-        for (int i = 0; i < data.pwmInputLevels.length; i++) {
-            data.pwmInputLevels[i] = buffer.get();
+        for (int i = 0; i < magnetHard.length; i++) {
+            magnetHard[i] = buffer.getFloat();
         }
 
-        data.boardType = buffer.getInt();
-        data.flagsValue = buffer.getInt();
-        data.crcValue = buffer.getInt();
+        altimeterSetting = buffer.getFloat();
+        temperatureSetting = buffer.getFloat();
 
-        flags = new Flags(32, data.flagsValue);
+        for (int i = 0; i < radioLevels.length; i++) {
+            radioLevels[i] = buffer.getFloat();
+        }
+        for (int i = 0; i < pwmInputLevels.length; i++) {
+            pwmInputLevels[i] = buffer.get();
+        }
+
+        boardType = buffer.getInt();
+        flags.setFlags(buffer.getInt());
+        crcValue = buffer.getInt();
     }
 
     @Override
@@ -70,115 +85,93 @@ public class CalibrationSettings implements SignalPayloadData {
         return SignalData.Command.CALIBRATION_SETTINGS_DATA;
     }
 
+    private int computeCrc() {
+        // compute CRC value from whole data, excluding last 4 bytes (CRC value)
+        return CommMessage.computeCrc32(Arrays.copyOfRange(serialize(), 0, getDataArraySize() - 4));
+    }
+
     public boolean isValid() {
-        return data.crcValue == CommMessage.computeCrc32(data.serialize());
+        return crcValue == computeCrc();
     }
 
     public ArrayList<CommMessage> getMessages() {
-        return CommMessage.buildMessagesList(getDataType(), data.serialize());
+        return CommMessage.buildMessagesList(getDataType(), serialize());
     }
 
-    private class Data {
-        float[] gyroOffset; // Vect3Df
-        float[] accelCalib; // Mat3Df
-        float[] magnetSoft; // Mat3Df
-        float[] magnetHard; // Vect3Df
-        float altimeterSetting;
-        float temperatureSetting;
-        float[] radioLevels; // Mat4Df
-        byte[] pwmInputLevels; // 8 params
-        int boardType;
-        int flagsValue;
-        int crcValue;
-
-        Data() {
-            gyroOffset = new float[3];
-            accelCalib = new float[9];
-            magnetSoft = new float[9];
-            magnetHard = new float[3];
-            radioLevels = new float[16];
-            pwmInputLevels = new byte[8];
-        }
-
-        int getDataArraySize() {
-            return 198;
-        }
-
-        byte[] serialize() {
-            byte[] dataArray = new byte[getDataArraySize()];
-            ByteBuffer buffer = ByteBuffer.allocate(dataArray.length);
-            buffer.order(ByteOrder.LITTLE_ENDIAN);
-
-            for (int i = 0; i < data.gyroOffset.length; i++) {
-                buffer.putFloat(data.gyroOffset[i]);
-            }
-            for (int i = 0; i < data.accelCalib.length; i++) {
-                buffer.putFloat(data.accelCalib[i]);
-            }
-            for (int i = 0; i < data.magnetSoft.length; i++) {
-                buffer.putFloat(data.magnetSoft[i]);
-            }
-            for (int i = 0; i < data.magnetHard.length; i++) {
-                buffer.putFloat(data.magnetHard[i]);
-            }
-
-            buffer.putFloat(data.altimeterSetting);
-            buffer.putFloat(data.temperatureSetting);
-
-            for (int i = 0; i < data.radioLevels.length; i++) {
-                buffer.putFloat(data.radioLevels[i]);
-            }
-            for (int i = 0; i < data.pwmInputLevels.length; i++) {
-                buffer.put(data.pwmInputLevels[i]);
-            }
-
-            buffer.putInt(data.boardType);
-            buffer.putInt(data.flagsValue);
-            buffer.putInt(data.crcValue);
-
-            System.arraycopy(buffer.array(), 0, dataArray, 0, dataArray.length);
-            return dataArray;
-        }
+    private int getDataArraySize() {
+        return 188;
     }
 
     public byte[] serialize() {
-        return data.serialize();
+        byte[] dataArray = new byte[getDataArraySize()];
+        ByteBuffer buffer = ByteBuffer.allocate(dataArray.length);
+        buffer.order(ByteOrder.LITTLE_ENDIAN);
+
+        for (float aGyroOffset : gyroOffset) {
+            buffer.putFloat(aGyroOffset);
+        }
+        for (float anAccelCalib : accelCalib) {
+            buffer.putFloat(anAccelCalib);
+        }
+        for (float aMagnetSoft : magnetSoft) {
+            buffer.putFloat(aMagnetSoft);
+        }
+        for (float aMagnetHard : magnetHard) {
+            buffer.putFloat(aMagnetHard);
+        }
+
+        buffer.putFloat(altimeterSetting);
+        buffer.putFloat(temperatureSetting);
+
+        for (float radioLevel : radioLevels) {
+            buffer.putFloat(radioLevel);
+        }
+        for (byte pwmInputLevel : pwmInputLevels) {
+            buffer.put(pwmInputLevel);
+        }
+
+        buffer.putInt(boardType);
+        buffer.putInt(flags.getFlags());
+        buffer.putInt(crcValue);
+
+        System.arraycopy(buffer.array(), 0, dataArray, 0, dataArray.length);
+        return dataArray;
     }
 
     public float[] getGyroOffset() {
-        return data.gyroOffset;
+        return gyroOffset;
     }
 
     public float[] getAccelCalib() {
-        return data.accelCalib;
+        return accelCalib;
     }
 
     public float[] getMagnetSoft() {
-        return data.magnetSoft;
+        return magnetSoft;
     }
 
     public float[] getMagnetHard() {
-        return data.magnetHard;
+        return magnetHard;
     }
 
     public float getAltimeterSetting() {
-        return data.altimeterSetting;
+        return altimeterSetting;
     }
 
     public float getTemperatureSetting() {
-        return data.temperatureSetting;
+        return temperatureSetting;
     }
 
     public float[] getRadioLevels() {
-        return data.radioLevels;
+        return radioLevels;
     }
 
     public byte[] getPwmInputLevels() {
-        return data.pwmInputLevels;
+        return pwmInputLevels;
     }
 
     public int getBoardType() {
-        return data.boardType;
+        return boardType;
     }
 
     public Flags getFlags() {
